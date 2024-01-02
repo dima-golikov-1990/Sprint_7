a90580ff77d4b739a5fc6f0d22a4cfd1677421f2
dima-golikov-1990 <145390552+dima-golikov-1990@users.noreply.github.com>
2024-01-02 19:23:53 +0300

    Create conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+BASE_URL = "https://qa-scooter.praktikum-services.ru/"
+
+PATH_COURIER = "api/v1/courier"
+PATH_LOGIN_COURIER = "api/v1/courier/login"
+PATH_ORDER = "api/v1/orders"
+
+HEADERS = {"accept": "application/json", "Content-Type": "application/json"}
