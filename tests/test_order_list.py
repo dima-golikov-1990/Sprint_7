12997c978fd33ad6fa11db7aa4424009f9882604
dima-golikov-1990 <145390552+dima-golikov-1990@users.noreply.github.com>
2024-01-02 19:26:54 +0300

    Update test_order_list.py

--- tests/test_order_list.py
+++ tests/test_order_list.py
@@ -2,8 +2,7 @@ import requests
 import json
 import allure
 
-base_url = "https://qa-scooter.praktikum-services.ru/"
-path = "api/v1/orders"
+from conftest import BASE_URL, PATH_ORDER, HEADERS
 
 fields_in_order = {"id", "courierId", "firstName", "lastName", "address", "metroStation", "phone", "rentTime", "deliveryDate", "track", "color", "comment", "createdAt", "updatedAt", "status"}
 
@@ -11,11 +10,13 @@ class TestOrderList:
     @allure.title('Проверка, что при запросе списка заказов возвращается список')
     @allure.description('Тест проверяет, что при запросе списка заказов возвращается список')
     def test_answer_body_contains_order_list(self):
-        headers = {"accept": "application/json", "Content-Type": "application/json"}
         payload = {}
-        response_raw = requests.get(url=base_url+path,
-                                     headers=headers,
+
+        response_raw = requests.get(url=BASE_URL+PATH_ORDER,
+                                     headers=HEADERS,
                                      data=json.dumps(payload))
         response_json = json.loads(response_raw.text)
+
         assert isinstance(response_json['orders'], list) == True
         assert response_raw.status_code == 200
+        
