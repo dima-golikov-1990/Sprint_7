07a0ecf67d9215f657bbe652fafd56ea3318af42
dima-golikov-1990 <145390552+dima-golikov-1990@users.noreply.github.com>
2024-01-02 19:26:01 +0300

    Update test_create_order.py

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -1,10 +1,9 @@
-import requests
 import json
 import allure
 import pytest
 
-base_url = "https://qa-scooter.praktikum-services.ru/"
-path = "api/v1/orders"
+from conftest import BASE_URL, PATH_ORDER, HEADERS
+from req_post import req_post
 
 class TestCreateOrder:
     @pytest.mark.parametrize(
@@ -16,30 +15,28 @@ class TestCreateOrder:
     @allure.title('Проверка что при создании заказа можно указать цвет BLACK и GREY')
     @allure.description('Тест проверяет что при создании заказа с цветами BLACK и GREY в ответе код 201')
     def test_create_order_with_color(self, color):
-        headers = {"accept": "application/json", "Content-Type": "application/json"}
         payload = {"color": [""+ color +""]}
-        response_raw = requests.post(url=base_url+path,
-                                     headers=headers,
-                                     data=json.dumps(payload))
+
+        response_raw = req_post(BASE_URL+PATH_ORDER, HEADERS, payload)
+
         assert response_raw.status_code == 201
 
     @allure.title('Проверка, что при создании заказа можно не указывать цвет')
     @allure.description('Тест проверяет что при создании заказа без указания цвет заказ выдаётся статус 201')
     def test_create_order_without_color(self):
-        headers = {"accept": "application/json", "Content-Type": "application/json"}
         payload = {}
-        response_raw = requests.post(url=base_url + path,
-                                     headers=headers,
-                                     data=json.dumps(payload))
+
+        response_raw = req_post(BASE_URL + PATH_ORDER, HEADERS, payload)
+
         assert response_raw.status_code == 201
 
     @allure.title('Проверка, что при создании заказа ответ содержит track')
     @allure.description('Тест проверяет что при создании заказа ответ содержит track')
     def test_answer_contains_track(self):
-        headers = {"accept": "application/json", "Content-Type": "application/json"}
         payload = {}
-        response_raw = requests.post(url=base_url + path,
-                                     headers=headers,
-                                     data=json.dumps(payload))
+
+        response_raw = req_post(BASE_URL + PATH_ORDER, HEADERS, payload)
+
         response_json = json.loads(response_raw.text)
         assert "track" in response_json
+        
